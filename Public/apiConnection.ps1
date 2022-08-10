f6c333981bac4626517df745e6f618a4dfaaf0ce
Aaron Glenn <aaron@aaroneg.com>
2022-08-10 17:41:52 -0500

    basic connection definitions

---
+++ Public/apiConnection.ps1
@@ -0,0 +1,36 @@
+function New-NBConnection {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$DeviceAddress,
+		[Parameter(Mandatory=$true,Position=1)][string]$ApiKey,
+		[Parameter(Mandatory=$false)][string]$SkipCertificateCheck,
+		[Parameter(Mandatory=$false)][switch]$Passthru
+	)
+	$ConnectionProperties = @{
+		Address = "$DeviceAddress"
+		ApiKey = $ApiKey
+		ApiBaseUrl = "https://$($DeviceAddress)/api"
+	}
+	$Connection = New-Object psobject -Property $ConnectionProperties
+	Write-Verbose "[$($MyInvocation.MyCommand.Name)] Host '$($Connection.Address)' is now the default connection."
+	$Script:Connection = $Connection
+	if ($Passthru) {
+		$Connection
+	}
+}
+function Test-NBConnection {
+	[CmdletBinding()]
+	param (
+		[Parameter(ValueFromPipeline=$true,Mandatory=$false,Position=0)][object]$NBConnection
+	)
+	if (!($NBConnection)) { 
+		Write-Verbose "[$($MyInvocation.MyCommand.Name)] Trying Default Connection"
+	}
+
+	$restParams=@{
+		Method = 'Get'
+		Uri = "$($NBConnection.ApiBaseUrl)/"
+	}
+
+}
+
