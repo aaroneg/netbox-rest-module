157deecdb88e88f0015dbb48827de3d51e25dd01
Aaron Glenn <aaron@aaroneg.com>
2022-08-11 23:26:17 -0700

    partial support for rack elevations

---
+++ Public/endpoints/dcim/rack-reservations.ps1
@@ -0,0 +1,105 @@
+Class NBRackReservation {
+	[int]$rack
+	[string]$units
+	[array]$user
+	[string]$description
+	# Constructor
+	NBRackReservation(
+		[int]$rack,
+		[array]$units,
+		[int]$user,
+		[string]$description
+
+	){
+		$this.rack = $rack
+		$this.units = $units
+		$this.user = $user
+		$this.description = $description
+	}
+}
+$RackReservationsAPIPath="dcim/rack-reservations"
+
+# function New-NBRackReservation {
+# 	[CmdletBinding()]
+# 	param (
+# 		[Parameter(Mandatory=$true,Position=0)][int]$rackid,
+# 		[Parameter(Mandatory=$true,Position=1)][array]$units,
+# 		[Parameter(Mandatory=$true,Position=3)][int]$user,
+# 		[Parameter(Mandatory=$true,Position=4)][string]$description,
+# 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+# 	)
+# 	$RackReservation=[NBRackReservation]::New($rackid,$units,$user,$description)
+# 	$restParams=@{
+# 		Method = 'Post'
+# 		URI = "$($Connection.ApiBaseURL)/$RackReservationsAPIPath/"
+# 		body = $RackReservation|ConvertTo-Json -Depth 50
+# 	}
+# 	$RackReservation|ConvertTo-Json -Depth 50
+# 	$RackReservation=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+# 	$RackReservation
+# }
+
+function Get-NBRackReservations {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$RackReservationsAPIPath/"
+	}
+	$restParams.URI
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection).results
+}
+
+function Get-NBRackReservation {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$RackReservationsAPIPath/$id/"
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+# function Set-NBRackReservation {
+# 	[CmdletBinding()]
+# 	param (
+# 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+# 		[Parameter(Mandatory=$true,Position=0)][int]$id,
+# 		[Parameter(Mandatory=$true,Position=1)][string]
+# 			[ValidateSet('name','slug','status','region','group','tenant','facility','time_zone','description','physical_address','shipping_address','latitude','longitude','comments')]
+# 			$key,
+# 		[Parameter(Mandatory=$true,Position=2)][string]$value
+# 	)
+# 	switch($key){
+# 		'slug' {$value=makeSlug -name $value}
+# 		default {}
+# 	}
+# 	$update=@{
+# 		$key = $value
+# 	}
+# 	$restParams=@{
+# 		Method = 'Patch'
+# 		URI = "$($Connection.ApiBaseURL)/$RackReservationsAPIPath/$id/"
+# 		body = $update | ConvertTo-Json -Depth 50
+# 	}
+# 	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+# }
+
+function Remove-NBRackReservation {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$RackReservationsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
