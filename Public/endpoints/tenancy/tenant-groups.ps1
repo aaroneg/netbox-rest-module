6e5d69c7d52ceb8c14db3228b8397cd0e32bdc39
Aaron Glenn <aaron@aaroneg.com>
2022-08-11 23:49:48 -0700

    tenants & groups

---
+++ Public/endpoints/tenancy/tenant-groups.ps1
@@ -0,0 +1,94 @@
+Class NBTenantGroup {
+	[string]$name
+	[string]$slug
+	# Constructor
+	NBTenantGroup(
+		[string]$name
+	){
+		$this.name = $name
+		$this.slug = makeSlug -name $name
+	}
+}
+$TenantGroupsAPIPath="tenancy/tenant-groups"
+
+function New-NBTenantGroup {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$Tenant=[NBTenantGroup]::New($name)
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$TenantGroupsAPIPath/"
+		body = $Tenant|ConvertTo-Json -Depth 50
+	}
+	Write-Verbose $Tenant|ConvertTo-Json -Depth 50
+	$Tenant=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$Tenant
+}
+
+function Get-NBTenantGroups {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$TenantGroupsAPIPath/"
+	}
+	$restParams.URI
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection).results
+}
+
+function Get-NBTenantGroup {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$TenantGroupsAPIPath/$id/"
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Set-NBTenantGroups {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('name','slug','parent','description')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$TenantGroupsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Remove-NBTenantGroup {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$TenantGroupsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
---
+++ Public/endpoints/tenancy/tenants.ps1
@@ -0,0 +1,94 @@
+Class NBTenant {
+	[string]$name
+	[string]$slug
+	# Constructor
+	NBTenant(
+		[string]$name
+	){
+		$this.name = $name
+		$this.slug = makeSlug -name $name
+	}
+}
+$TenantsAPIPath="tenancy/tenants"
+
+function New-NBTenant {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$Tenant=[NBTenant]::New($name)
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/"
+		body = $Tenant|ConvertTo-Json -Depth 50
+	}
+	Write-Verbose $Tenant|ConvertTo-Json -Depth 50
+	$Tenant=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$Tenant
+}
+
+function Get-NBTenants {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/"
+	}
+	$restParams.URI
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection).results
+}
+
+function Get-NBTenant {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Get'
+		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/$id/"
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Set-NBTenant {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('name','slug','group','description','comments')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Remove-NBTenant {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
