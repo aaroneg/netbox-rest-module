6e8d75bfbd16bd3c6f226a59875f6f71986955f1
Aaron Glenn <aaron@aaroneg.com>
2022-08-15 23:34:17 -0700

    add support for IP prefixes

---
+++ Public/endpoints/ipam/prefixes.ps1
@@ -0,0 +1,79 @@
+Class NBPrefix {
+	[string]$prefix
+	# Constructor
+	NBPrefix(
+		[string]$prefix
+	){
+		$this.prefix = $prefix
+	}
+}
+$SitesAPIPath="ipam/prefixes"
+
+function New-NBPrefix {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$prefix,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$PostObject=[NBPrefix]::New($prefix)
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$SitesAPIPath/"
+		body = $PostObject|ConvertTo-Json -Depth 50
+	}
+	Write-Verbose $PostObject|ConvertTo-Json -Depth 50
+	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$PostObject
+}
+
+function Get-NBPrefixes {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	Get-ApiItems -apiConnection $Connection -RelativePath $SitesAPIPath
+}
+
+function Get-NBPrefixByID {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	Get-ApiItemByID -apiConnection $Connection -RelativePath $SitesAPIPath -id $id
+}
+function Set-NBPrefix {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('prefix','site','vrf','tenant','vlan','role','is_pool','mark_utilized',
+			'description')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$SitesAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Remove-NBPrefix {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$SitesAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
