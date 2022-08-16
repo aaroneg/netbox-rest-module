dbb7da570b832a53fe0381caaa204f83f4edb447
Aaron Glenn <aaron@aaroneg.com>
2022-08-15 23:10:31 -0700

    support vm interfaces

---
+++ Public/endpoints/virtualization/interfaces.ps1
@@ -0,0 +1,97 @@
+Class NBVMInterface {
+	[int]$virtual_machine
+	[string]$name
+	# Constructor
+	NBVMInterface(
+		[int]$virtual_machine,
+		[string]$name
+	){
+		$this.virtual_machine = $virtual_machine
+		$this.name = $name
+	}
+}
+$VirtualizationInterfaceAPIPath="virtualization/interfaces"
+
+function New-NBVMInterface {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][int]$VMID,
+		[Parameter(Mandatory=$true,Position=1)][string]$name,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$PostObject=[NBVMInterface]::New($VMID,$name)
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationInterfaceAPIPath/"
+		body = $PostObject|ConvertTo-Json -Depth 50
+	}
+	Write-Verbose $PostObject|ConvertTo-Json -Depth 50
+	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$PostObject
+}
+
+function Get-NBVMInterfaces {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	Get-ApiItems -apiConnection $Connection -RelativePath $VirtualizationInterfaceAPIPath
+}
+
+function Get-NBVMInterfaceByID {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	Get-ApiItemByID -apiConnection $Connection -RelativePath $VirtualizationInterfaceAPIPath -id $id
+}
+
+function Find-NBVMInterfacesByName {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=1)][string]$name
+	)
+	Find-ApiItemsByName -apiConnection $Connection -RelativePath $VirtualizationInterfaceAPIPath -name $name
+}
+
+function Set-NBVMInterface {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('virtual_machine','name','enabled','parent','bridge','mtu','mac_address','description',
+			'mode','untagged_vlan','vrf')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationInterfaceAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Remove-NBVMInterface {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationInterfaceAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
