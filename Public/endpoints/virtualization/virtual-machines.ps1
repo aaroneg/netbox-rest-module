4162c2c86e142e59bd68c55a8558fe9b28e62adf
Aaron Glenn <aaron@aaroneg.com>
2022-08-15 22:51:59 -0700

    support virtual machines

---
+++ Public/endpoints/virtualization/virtual-machines.ps1
@@ -0,0 +1,96 @@
+Class NBVM {
+	[string]$name
+	[int]$cluster
+	# Constructor
+	NBVM(
+		[string]$name,
+		[int]$cluster
+	){
+		$this.name = $name
+		$this.cluster = $cluster
+	}
+}
+$VirtualizationVMsAPIPath="virtualization/virtual-machines"
+
+function New-NBVM {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$true,Position=1)][int]$clusterID,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$PostObject=[NBVM]::New($name,$clusterID)
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationVMsAPIPath/"
+		body = $PostObject|ConvertTo-Json -Depth 50
+	}
+	Write-Verbose $PostObject|ConvertTo-Json -Depth 50
+	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	$PostObject
+}
+
+function Get-NBVMs {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	Get-ApiItems -apiConnection $Connection -RelativePath $VirtualizationVMsAPIPath
+}
+
+function Get-NBVMByID {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	Get-ApiItemByID -apiConnection $Connection -RelativePath $VirtualizationVMsAPIPath -id $id
+}
+
+function Find-NBVMsByName {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=1)][string]$name
+	)
+	Find-ApiItemsByName -apiConnection $Connection -RelativePath $VirtualizationVMsAPIPath -name $name
+}
+
+function Set-NBVM {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('name','slug','status','region','group','tenant','facility','time_zone','description','physical_address','shipping_address','latitude','longitude','comments')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationVMsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
+
+function Remove-NBVM {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	$restParams=@{
+		Method = 'Delete'
+		URI = "$($Connection.ApiBaseURL)/$VirtualizationVMsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+}
