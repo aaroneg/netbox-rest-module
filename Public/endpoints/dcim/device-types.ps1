472e6e06241ba4b9340beefc144007a395209289
Aaron Glenn <aaron@aaroneg.com>
2022-08-17 19:04:05 -0500

    fix result

--- Public/endpoints/dcim/device-types.ps1
+++ Public/endpoints/dcim/device-types.ps1
@@ -55,7 +55,7 @@ function Get-NBDeviceTypeByModel {
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][string]$model
 	)
-	Get-ApiItemByQuery -apiConnection $Connection -RelativePath $deviceTypesPath -field 'model__ie' -value $model
+	(Get-ApiItemByQuery -apiConnection $Connection -RelativePath $deviceTypesPath -field 'model__ie' -value $model).results
 }
 
 
