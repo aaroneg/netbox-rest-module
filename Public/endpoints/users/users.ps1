ed7282e842dcfc427ef2c9a4987f7b796689b267
Aaron Glenn <aaron@aaroneg.com>
2022-09-01 16:30:06 -0500

    cleanup

--- Public/endpoints/users/users.ps1
+++ Public/endpoints/users/users.ps1
@@ -17,30 +17,14 @@ function Get-NBUserByID {
 	Get-ApiItemByID -apiConnection $Connection -RelativePath $UsersAPIPath -id $id
 }
 
-# function Set-NBUser {
-# 	[CmdletBinding()]
-# 	param (
-# 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
-# 		[Parameter(Mandatory=$true,Position=0)][int]$id,
-# 		[Parameter(Mandatory=$true,Position=1)][string]
-# 			[ValidateSet('name','slug','status','region','group','tenant','facility','time_zone','description','physical_address','shipping_address','latitude','longitude','comments')]
-# 			$key,
-# 		[Parameter(Mandatory=$true,Position=2)][string]$value
-# 	)
-# 	switch($key){
-# 		'slug' {$value=makeSlug -name $value}
-# 		default {}
-# 	}
-# 	$update=@{
-# 		$key = $value
-# 	}
-# 	$restParams=@{
-# 		Method = 'Patch'
-# 		URI = "$($Connection.ApiBaseURL)/$UsersAPIPath/$id/"
-# 		body = $update | ConvertTo-Json -Depth 50
-# 	}
-# 	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
-# }
+function Get-NBUserByName {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][string]$name
+	)
+	Get-ApiItemByName -apiConnection $Connection -RelativePath $UsersAPIPath -value $name
+}
 
 # function Remove-NBUser {
 # 	[CmdletBinding()]
