67e5f14e91de0c6ae0089c5cc2e1f507daf32939
Aaron Glenn <aaron@aaroneg.com>
2022-09-01 14:01:21 -0500

    add get by name function

--- Public/endpoints/virtualization/cluster-types.ps1
+++ Public/endpoints/virtualization/cluster-types.ps1
@@ -45,6 +45,15 @@ function Get-NBVMClusterTypeByID {
 	Get-ApiItemByID -apiConnection $Connection -RelativePath $VirtualizationClusterTypesAPIPath -id $id
 }
 
+function Get-NBVMClusterTypeByName {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][string]$name
+	)
+	Get-ApiItemByName -apiConnection $Connection -RelativePath $VirtualizationClusterTypesAPIPath -value $name
+}
+
 function Find-NBVMClusterTypesContainingName {
 	[CmdletBinding()]
 	param (
