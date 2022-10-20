140c6841761afa05ca8b5a139462ca1de3e00abd
Aaron Glenn <aaron@aaroneg.com>
2022-10-20 14:02:45 -0500

    "

--- Public/endpoints/tenancy/tenants.ps1
+++ Public/endpoints/tenancy/tenants.ps1
@@ -1,27 +1,34 @@
-Class NBTenant {
-	[string]$name
-	[string]$slug
-	# Constructor
-	NBTenant(
-		[string]$name
-	){
-		$this.name = $name
-		$this.slug = makeSlug -name $name
-	}
-}
 $TenantsAPIPath="tenancy/tenants"
 
 function New-NBTenant {
+	<#
+	.SYNOPSIS
+	Adds a new tenant to Netbox
+	.PARAMETER name
+	This parameter will be used both directly and to create an appropriate slug.
+	.PARAMETER group
+	Group object ID
+	.PARAMETER description
+	Any description you'd like to add
+	.PARAMETER comments
+	Any comments you'd like to add
+	.PARAMETER Connection
+	Connection object to use
+	#>
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][int]$group,
+		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
-	$PostObject=[NBTenant]::New($name)
+	$PSBoundParameters['slug']=makeSlug -name $name
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
 	$restParams=@{
 		Method = 'Post'
 		URI = "$($Connection.ApiBaseURL)/$TenantsAPIPath/"
-		body = $PostObject|ConvertTo-Json -Depth 50
+		body = $PostJson
 	}
 	Write-Verbose $PostObject|ConvertTo-Json -Depth 50
 	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
