064f38e8363af9f7860142891a754173d830cd0f
aaron <aaron@aaroneg.com>
2023-05-26 17:46:38 -0500

    upgrade new-nbvrf

--- Public/endpoints/ipam/vrfs.ps1
+++ Public/endpoints/ipam/vrfs.ps1
@@ -1,25 +1,40 @@
-Class NBVRF {
-	[string]$name
-	# Constructor
-	NBVRF(
-		[string]$name
-	){
-		$this.name = $name
-	}
-}
+
 $VRFsApiPath="ipam/vrfs"
 
 function New-NBVRF {
+	<#
+	.SYNOPSIS
+	Creates a VRF object
+	.PARAMETER name
+	Name of the object
+	.PARAMETER rd
+	Route distinguisher
+	.PARAMETER tenant
+	Tenant id
+	.PARAMETER enforce_unique
+	Enforce unique IP addresses in this VRF
+	.PARAMETER description
+	Description
+	.PARAMETER comments
+	Comments
+	.PARAMETER Connection
+	Connection object
+	#>
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][string]$rd,
+		[Parameter(Mandatory=$false)][int]$tenant,
+		[Parameter(Mandatory=$false)][bool]$enforce_unique,
+		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
-	$PostObject=[NBVRF]::New($name)
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
 	$restParams=@{
 		Method = 'Post'
 		URI = "$($Connection.ApiBaseURL)/$VRFsApiPath/"
-		body = $PostObject|ConvertTo-Json -Depth 50
+		body = $PostJson
 	}
 	
 	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
