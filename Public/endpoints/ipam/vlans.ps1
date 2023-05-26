5dbd4ccb0851e91f76ef73503876a48fecae597c
aaron <aaron@aaroneg.com>
2023-05-26 17:35:49 -0500

    updates to syntax

--- Public/endpoints/ipam/vlans.ps1
+++ Public/endpoints/ipam/vlans.ps1
@@ -1,54 +1,52 @@
-Class NBVLAN {
-	[int]$vid
-	[string]$name
-	[string]$status
-	[int]$site
-	# [int]$group
-	[int]$tenant
-	# [int]$role
-	# [string]$description
-	# Constructor
-	NBVLAN(
-		[int]$vid,
-		[string]$name,
-		[string]$status
-	){
-		$this.vid = $vid
-		$this.name = $name
-		$this.status = $status
-	}
-}
 $VLANsAPIPath="ipam/vlans"
 
 function New-NBVLAN {
+	<#
+	.SYNOPSIS
+	Creates a vlan object
+	.PARAMETER name
+	Name of the object
+	.PARAMETER vid 
+	vlan id number
+	.PARAMETER status
+	status of the vlan
+	.PARAMETER site
+	site id
+	.PARAMETER group
+	group id
+	.PARAMETER tenant
+	tenant id
+	.PARAMETER role
+	role id
+	.PARAMETER description
+	vlan description
+	.PARAMETER comments
+	comments
+	.PARAMETER Connection
+	connection object
+	#>
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
 		[Parameter(Mandatory=$true,Position=1)][int]$vid,
-		[Parameter(Mandatory=$true,Position=2)]
+		[Parameter(Mandatory=$false,Position=2)]
 			[ValidateSet('active','reserved','deprecated')]
-			[string]$status,
-		[Parameter(Mandatory=$false)][int]$siteID,
-		[Parameter(Mandatory=$false)][int]$groupID,
-		[Parameter(Mandatory=$false)][int]$tenantID,
-		[Parameter(Mandatory=$false)][int]$roleID,
+			[string]$status="active",
+		[Parameter(Mandatory=$false)][int]$site,
+		[Parameter(Mandatory=$false)][int]$group,
+		[Parameter(Mandatory=$false)][int]$tenant,
+		[Parameter(Mandatory=$false)][int]$role,
 		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
-	$PostObject=[NBVLAN]::New($vid,$name,$status)
-	if($siteID) {$PostObject.site = $siteID}
-	if($groupID) {
-		$PostObject | Add-Member -MemberType NoteProperty -Name group -Value $groupID 
-	}
-	if($tenantID) {$PostObject.tenant = $tenantID}
-	if($roleID) {$PostObject.role = $roleID}
-	if($description) {$PostObject.description = $description}
+	if (!($PSBoundParameters.ContainsKey('status'))) {$PSBoundParameters.add('status', $status)}
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
 	$restParams=@{
 		Method = 'Post'
 		URI = "$($Connection.ApiBaseURL)/$VLANsAPIPath/"
-		body = $PostObject|ConvertTo-Json -Depth 50
+		body = $PostJson
 	}
-	
 	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
 	if ($PostObject.message) {
 		throw $PostObject.message
