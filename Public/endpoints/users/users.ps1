28d799f6d0f6b7b5af8a7db6be7e5cad06bec8e0
Aaron <aaron@aaroneg.com>
2023-11-09 20:34:21 -0600

    remove unused code

--- Public/endpoints/users/users.ps1
+++ Public/endpoints/users/users.ps1
@@ -26,18 +26,4 @@ function Get-NBUserByName {
 	Get-ApiItemByName -apiConnection $Connection -RelativePath $UsersAPIPath -value $name
 }
 
-# function Remove-NBUser {
-# 	[CmdletBinding()]
-# 	param (
-# 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
-# 		[Parameter(Mandatory=$true,Position=0)][int]$id
-# 	)
-# 	$restParams=@{
-# 		Method = 'Delete'
-# 		URI = "$($Connection.ApiBaseURL)/$UsersAPIPath/$id/"
-# 		body = $update | ConvertTo-Json -Depth 50
-# 	}
-# 	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
-# }
-
 Export-ModuleMember -Function "*-*"
