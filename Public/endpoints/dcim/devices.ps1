56bf70a63ab104c1710f84c59096ed3c84f96ac6
Aaron <aaron@aaroneg.com>
2023-11-09 20:26:29 -0600

    enhance new-nbdevice

--- Public/endpoints/dcim/devices.ps1
+++ Public/endpoints/dcim/devices.ps1
@@ -1,45 +1,52 @@
-Class NBDevice {
-	[string]$name
-	[int]$device_type
-	[int]$device_role
-	[int]$site
-	[string]$face
-	# Constructor
-	NBDevice(
-		[string]$name,
-		[int]$device_type,
-		[int]$device_role,
-		[int]$site,
-		[string]$face
-	){
-		$this.name = $name
-		$this.device_type = $device_type
-		$this.device_role = $device_role
-		$this.site = $site
-		$this.face = $face
-	}
-}
 $DevicesAPIPath="dcim/devices"
 
 function New-NBDevice {
+	<#
+	.SYNOPSIS
+	Adds a new device object to Netbox
+	#>
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
-		[Parameter(Mandatory=$true,Position=1)][int]$device_typeID,
-		[Parameter(Mandatory=$true,Position=2)][int]$device_roleID,
-		[Parameter(Mandatory=$true,Position=3)][int]$siteID,
-		# [Parameter(Mandatory=$true,Position=4)][string]
-		# 	[ValidateSet('front','rear')]
-		# 	$face,
+		[Parameter(Mandatory=$true,Position=1)][int]$device_type,
+		[Parameter(Mandatory=$true,Position=2)][int]$role,
+		[Parameter(Mandatory=$true,Position=3)][int]$tenant,
+		[Parameter(Mandatory=$false)][int]$platform,
+		[Parameter(Mandatory=$false)][string]$serial,
+		[Parameter(Mandatory=$false)][string]$asset_tag,
+		[Parameter(Mandatory=$false)][int]$site,
+		[Parameter(Mandatory=$false)][int]$location,
+		[Parameter(Mandatory=$false)][int]$rack,
+		[Parameter(Mandatory=$false)][int]$postition,
+		[Parameter(Mandatory=$false)]
+			[ValidateSet('front','rear')]
+			[string]$face,
+		[Parameter(Mandatory=$false)][int]$latitude,
+		[Parameter(Mandatory=$false)][int]$longitude,
+		[Parameter(Mandatory=$false)]
+			[ValidateSet('offline','active','planned','staged','failed','inventory','decommissioning')]
+			[string]$status,
+		[Parameter(Mandatory=$false)]
+			[ValidateSet('front-to-rear','rear-to-front','left-to-right','right-to-left','side-to-rear','passive','mixed')]
+			[string]$airflow,
+		[Parameter(Mandatory=$false)][int]$primary_ip4,
+		[Parameter(Mandatory=$false)][int]$primary_ip6,
+		[Parameter(Mandatory=$false)][int]$oop_ip,
+		[Parameter(Mandatory=$false)][int]$cluster,
+		[Parameter(Mandatory=$false)][int]$virtual_chassis,
+		[Parameter(Mandatory=$false)][int]$vc_position,
+		[Parameter(Mandatory=$false)][int]$vc_priority,
+		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][string]$comments,
+		[Parameter(Mandatory=$false)][int]$config_template,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
-	$PostObject=[NBDevice]::New($name,$device_typeID,$device_roleID,$siteID,$face)
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
 	$restParams=@{
 		Method = 'Post'
 		URI = "$($Connection.ApiBaseURL)/$DevicesAPIPath/"
-		body = $PostObject|ConvertTo-Json -Depth 50
+		body = $PostJson
 	}
-	
 	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
 	if ($PostObject.message) {
 		throw $PostObject.message
