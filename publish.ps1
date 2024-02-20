9c7f5e6b266c6b9f2bea9847bf191282bf6d791a
Aaron <aaron@aaroneg.com>
2024-02-20 17:00:00 +0000

    fix publish script, update module template

--- publish.ps1
+++ publish.ps1
@@ -1,30 +1,9 @@
-try {Get-PSRepository 'LocalPSRepo' -ErrorAction Stop} 
-catch {
-	$registerPSRepositorySplat = @{
-		Name = 'LocalPSRepo'
-		SourceLocation = '\\localhost\nuget'
-		ScriptSourceLocation = '\\localhost\nuget'
-		InstallationPolicy = 'Trusted'
-	}
-	Register-PSRepository @registerPSRepositorySplat
-}
-# Publish to a file share repo - the NuGet API key must be a non-blank string
+. $PSScriptRoot\version.ps1
+
 $publishModuleSplat = @{
-    Path = "$PSScriptRoot\Build\netbox-rest-module\0.0.1"
-    Repository = 'LocalPsRepo'
-    NuGetApiKey = 'AnyStringWillDo'
+    Path = "$PSScriptRoot\Build\netbox-rest-module\$moduleVersionTarget"
+    Repository = 'PSGallery'
+
 }
-Publish-Module @publishModuleSplat -Force -Verbose -Debug
+Publish-Module @publishModuleSplat -NuGetApiKey (Read-Host -Prompt 'API Key')
 install-module netbox-rest-module -Scope CurrentUser -Force
-
-
-<#
-# Register a file share on my local machine
-$registerPSRepositorySplat = @{
-    Name = 'LocalPSRepo'
-    SourceLocation = '\\localhost\nuget'
-    ScriptSourceLocation = '\\localhost\nuget'
-    InstallationPolicy = 'Trusted'
-}
-Register-PSRepository @registerPSRepositorySplat
-#>
