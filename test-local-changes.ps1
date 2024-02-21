69502cde44a92a70616304c30928c132888d1a61
Aaron <aaron@aaroneg.com>
2024-02-21 02:22:36 +0000

    update test script

--- test-local-changes.ps1
+++ test-local-changes.ps1
@@ -1,7 +1,11 @@
 
-. .\version.ps1
+. $PSScriptRoot\version.ps1
 
 if ($IsLinux) {
+	if(!(test-path $env:HOME/.local/share/powershell/Modules/netbox-rest-module/$moduleVersionTarget)) {New-Item -ItemType Directory -Path ~/.local/share/powershell/Modules/netbox-rest-module/$moduleVersionTarget}
 	Copy-Item $PSScriptRoot\Build\netbox-rest-module\$moduleVersionTarget\* ~/.local/share/powershell/Modules/netbox-rest-module/$moduleVersionTarget/
 	Import-Module netbox-rest-module -Force
 }
+remove-module netbox-rest-module
+import-module netbox-rest-module -force
+Get-Module netbox-rest-module
