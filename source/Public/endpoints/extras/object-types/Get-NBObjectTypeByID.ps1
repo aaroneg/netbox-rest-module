65128a76f6360afb61081c1f226eb3d6337f4b33
Aaron Glenn <aaron@aaroneg.com>
2024-06-29 20:28:49 +0000

    Support listing object types

---
+++ source/Public/endpoints/extras/object-types/Get-NBObjectTypeByID.ps1
@@ -0,0 +1,9 @@
+function Get-NBObjectTypeByID {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id
+	)
+	Get-ApiItemByID -apiConnection $Connection -RelativePath $NBObjectTypesAPIPath -id $id
+
+}
