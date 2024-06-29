faad0eae1e0637c66edf8d2aa6dfc276c123c515
Aaron Glenn <aaron@aaroneg.com>
2024-06-29 20:28:14 +0000

    basic tag support

---
+++ source/Public/endpoints/extras/tags/New-NBTag.ps1
@@ -0,0 +1,24 @@
+function New-NBTag {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][string]$color,
+		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false,
+			HelpMessage="An array of strings in the format <applabel>.<model>, ex: 'vpn.tunnel','vpn.tunnelgroup' ")][string[]]$object_types,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$PSBoundParameters['slug']=makeSlug -name $name
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$NBTagsAPIPath/"
+		body = $PostJson
+	}
+	
+	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	if ($PostObject.message) {
+		throw $PostObject.message
+	}
+	$PostObject
+}
---
+++ source/Public/endpoints/extras/tags/Set-NBTag.ps1
@@ -0,0 +1,25 @@
+function Set-NBTag {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('name','slug','color','description','object_types')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2)][string]$value
+	)
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$NBTagsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+
+}
