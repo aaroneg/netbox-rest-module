a2d032b28c6313a1a8bdc7b3ce762bd704da0170
Aaron Glenn <aaron@aaroneg.com>
2024-06-29 20:03:25 +0000

    Add support for circuit providers

---
+++ source/Public/endpoints/circuits/providers/Get-NBCircuitProviders.ps1
@@ -0,0 +1,8 @@
+function Get-NBCircuitProviders {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	Get-ApiItems -apiConnection $Connection -RelativePath $NBCircuitProvidersAPIPath
+
+}
