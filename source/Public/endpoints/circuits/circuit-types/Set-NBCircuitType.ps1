9e43e2767de96f789376900cc6abbc2f805a0159
Aaron Glenn <aaron@aaroneg.com>
2024-06-29 21:38:32 +0000

    Enhance set-nbcircuit* to allow setting tags

--- source/Public/endpoints/circuits/circuit-types/Set-NBCircuitType.ps1
+++ source/Public/endpoints/circuits/circuit-types/Set-NBCircuitType.ps1
@@ -1,10 +1,10 @@
-function Set-NBCircuitProviderNetwork {
+function Set-NBCircuitType {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('name','provider','service_id','description','comments','tags')]
+			[ValidateSet('name','slug','color','description','tags')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2,
 			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
@@ -20,9 +20,11 @@ function Set-NBCircuitProviderNetwork {
 	}
 	$restParams=@{
 		Method = 'Patch'
-		URI = "$($Connection.ApiBaseURL)/$NBCircuitProviderNetworksAPIPath/$id/"
+		URI = "$($Connection.ApiBaseURL)/$NBCircuitTypesAPIPath/$id/"
 		body = $update | ConvertTo-Json -Depth 50
 	}
-	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+	$result = (Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+	if ($result.message) { $result.message }
+	else { $result }
 
 }
--- source/Public/endpoints/circuits/provider-accounts/Set-NBCircuitProviderAccount.ps1
+++ source/Public/endpoints/circuits/provider-accounts/Set-NBCircuitProviderAccount.ps1
@@ -4,12 +4,15 @@ function Set-NBCircuitProviderAccount {
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('name','account','description','provider')]
+			[ValidateSet('name','account','description','provider','tags')]
 			$key,
-		[Parameter(Mandatory=$true,Position=2)][string]$value
+		[Parameter(Mandatory=$true,Position=2,
+			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
+		)][string]$value
 	)
 	switch($key){
 		'slug' {$value=makeSlug -name $value}
+		'tags' {[array]$value=$value.Split(',')}
 		default {}
 	}
 	$update=@{
