aca58ed1232050f447df667788d7f3c9c38c90f5
Aaron <aaron@aaroneg.com>
2024-06-30 09:45:45 -0500

    add windows option

--- test-local-changes.ps1
+++ test-local-changes.ps1
@@ -6,6 +6,12 @@ if ($IsLinux) {
 	Copy-Item $PSScriptRoot\Build\netbox-rest-module\$moduleVersionTarget\* ~/.local/share/powershell/Modules/netbox-rest-module/$moduleVersionTarget/
 	Import-Module netbox-rest-module -Force
 }
+if ($IsWindows){
+	$UserModulePath=$env:PSModulePath.Split(';')[0]
+	if(!(Test-Path $UserModulePath\netbox-rest-module\$moduleVersionTarget)) {New-Item -ItemType Directory -Path $UserModulePath/netbox-rest-module/$moduleVersionTarget -Force}
+	Copy-Item $PSScriptRoot\Build\netbox-rest-module\$moduleVersionTarget\* $UserModulePath/netbox-rest-module/$moduleVersionTarget
+	Import-Module netbox-rest-module -Force
+}
 remove-module netbox-rest-module
 import-module netbox-rest-module -force
 Get-Module netbox-rest-module
