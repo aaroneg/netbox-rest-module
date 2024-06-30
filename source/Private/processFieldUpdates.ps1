4d8d4af009746b6acdf57383e2f7a2ebb55b1c02
Aaron Glenn <aaron@aaroneg.com>
2024-06-30 03:02:03 +0000

    add process function

---
+++ source/Private/processFieldUpdates.ps1
@@ -0,0 +1,13 @@
+function processFieldUpdates($key,$value) {
+	switch($key){
+		'slug' {$value=makeSlug -name $value}
+		'tags' {[array]$value=$value.Split(',')}
+		'install_date' {$value = $value|Get-Date -Format 'yyyy-MM-dd'}
+		'termination_date' {$value = $value|Get-Date -Format 'yyyy-MM-dd'}
+		default {}
+	}
+	$update=@{
+		$key = $value
+	}
+	$update
+}
