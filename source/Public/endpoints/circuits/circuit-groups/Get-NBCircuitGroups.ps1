2debd4da62e2bee34d3e730f426774e0f3bac5f3
Aaron <aaron@aaroneg.com>
2024-12-16 17:22:11 -0800

    support for getting circuit groups

---
+++ source/Public/endpoints/circuits/circuit-groups/Get-NBCircuitGroups.ps1
@@ -0,0 +1,7 @@
+function Get-NBCircuitGroups {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	Get-ApiItems -apiConnection $Connection -RelativePath $NBCircuitGroupsAPIPath
+}
