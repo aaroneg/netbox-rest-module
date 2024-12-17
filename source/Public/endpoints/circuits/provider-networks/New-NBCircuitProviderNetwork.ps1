768cf44bb92b99e8852821eff97474c57a6179fe
Aaron <aaron@aaroneg.com>
2024-12-16 17:45:28 -0800

    "

--- source/Public/endpoints/circuits/provider-networks/New-NBCircuitProviderNetwork.ps1
+++ source/Public/endpoints/circuits/provider-networks/New-NBCircuitProviderNetwork.ps1
@@ -1,12 +1,13 @@
 function New-NBCircuitProviderNetwork {
 	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory=$true,Position=0)][string]$name,
 		[Parameter(Mandatory=$true,Position=1)][int]$provider,
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
 		[Parameter(Mandatory=$false)][string]$service_id,
 		[Parameter(Mandatory=$false)][string]$description,
 		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][string[]]$tags,
+		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
 	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
--- source/Public/endpoints/circuits/provider-networks/Set-NBCircuitProviderNetwork.ps1
+++ source/Public/endpoints/circuits/provider-networks/Set-NBCircuitProviderNetwork.ps1
@@ -4,7 +4,7 @@ function Set-NBCircuitProviderNetwork {
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('name','provider','service_id','description','comments','tags')]
+			[ValidateSet('provider','name','service_id','description','comments','tags','custom_fields')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2,
 			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
