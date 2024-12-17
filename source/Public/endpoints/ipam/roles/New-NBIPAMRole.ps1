9836cca5b67d26fc3ee3131f15ae1bbafba94095
Aaron <aaron@aaroneg.com>
2024-12-17 11:19:25 -0800

    "

--- source/Public/endpoints/ipam/roles/New-NBIPAMRole.ps1
+++ source/Public/endpoints/ipam/roles/New-NBIPAMRole.ps1
@@ -10,7 +10,10 @@ function New-NBIPAMRole {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][int]$weight,
+		[Parameter(Mandatory=$false)][string]$description,
 		[Parameter(Mandatory=$false)][string[]]$tags,
+		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
 	$PSBoundParameters['slug']=makeSlug -name $name
