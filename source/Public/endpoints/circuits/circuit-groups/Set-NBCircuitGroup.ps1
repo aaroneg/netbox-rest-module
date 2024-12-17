5aaca91bc6a695e94442b49d22e2148ead44d40b
Aaron <aaron@aaroneg.com>
2024-12-16 17:29:22 -0800

    Add Set-NBCircuitGroup

---
+++ source/Public/endpoints/circuits/circuit-groups/Set-NBCircuitGroup.ps1
@@ -0,0 +1,22 @@
+function Set-NBCircuitGroup {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
+		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		# ValidateSets updated as of Netbox v4.1.8
+		[Parameter(Mandatory=$true,Position=1)][string]
+			[ValidateSet('name','slug','description','tenant','tags','custom_fields')]
+			$key,
+		[Parameter(Mandatory=$true,Position=2,
+			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
+		)][string]$value
+	)
+	$update=processFieldUpdates $key $value
+	$restParams=@{
+		Method = 'Patch'
+		URI = "$($Connection.ApiBaseURL)/$NBCircuitGroupsAPIPath/$id/"
+		body = $update | ConvertTo-Json -Depth 50
+	}
+	(Invoke-CustomRequest -restParams $restParams -Connection $Connection)
+
+}
