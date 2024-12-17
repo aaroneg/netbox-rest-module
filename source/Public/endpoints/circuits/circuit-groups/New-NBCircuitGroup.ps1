f3d94eb79d1478d176ed960232418e92f43ee99e
Aaron <aaron@aaroneg.com>
2024-12-16 17:16:48 -0800

    Support creating a new circuit group

---
+++ source/Public/endpoints/circuits/circuit-groups/New-NBCircuitGroup.ps1
@@ -0,0 +1,25 @@
+function New-NBCircuitGroup {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][int]$tenant,
+		[Parameter(Mandatory=$false)][string[]]$tags,
+		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
+		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
+	)
+	$PSBoundParameters['slug']=makeSlug -name $name
+	$PostJson = createPostJson -Fields ($PSBoundParameters.GetEnumerator())
+	$restParams=@{
+		Method = 'Post'
+		URI = "$($Connection.ApiBaseURL)/$NBCircuitGroupsAPIPath/"
+		body = $PostJson
+	}
+	
+	$PostObject=Invoke-CustomRequest -restParams $restParams -Connection $Connection
+	if ($PostObject.message) {
+		throw $PostObject.message
+	}
+	$PostObject
+
+}
