cd1f30c02b4ddb3b1ecb93d105423496b7d0c295
Aaron <aaron@aaroneg.com>
2024-12-16 17:36:05 -0800

    "

--- source/Public/endpoints/circuits/circuit-types/Set-NBCircuitType.ps1
+++ source/Public/endpoints/circuits/circuit-types/Set-NBCircuitType.ps1
@@ -4,7 +4,7 @@ function Set-NBCircuitType {
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('name','slug','color','description','tags')]
+			[ValidateSet('name','slug','color','description','custom_fields','tags')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2,
 			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
