50d2760f15fd1c97c1562bd4aa28c8d2f0ce577b
Aaron <aaron@aaroneg.com>
2024-12-16 18:00:37 -0800

    add ASN support

--- source/Private/processFieldUpdates.ps1
+++ source/Private/processFieldUpdates.ps1
@@ -2,6 +2,7 @@ function processFieldUpdates($key,$value) {
 	switch($key){
 		'slug' {$value=makeSlug -name $value}
 		'tags' {[array]$value=$value.Split(',')}
+		'asns' {[array]$value=$value.Split(',')}
 		'install_date' {$value = $value|Get-Date -Format 'yyyy-MM-dd'}
 		'termination_date' {$value = $value|Get-Date -Format 'yyyy-MM-dd'}
 		'ipaddresses' {[array]$value=$value.Split(',')}
