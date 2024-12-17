87db0a0972d04ac3dee30395f6a74eca4c00caec
Aaron <aaron@aaroneg.com>
2024-12-17 11:23:00 -0800

    "

--- source/Public/endpoints/ipam/service-templates/Set-NBServiceTemplate.ps1
+++ source/Public/endpoints/ipam/service-templates/Set-NBServiceTemplate.ps1
@@ -4,7 +4,7 @@ function Set-NBServiceTemplate {
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('name','protocol','ports','description','comments','tags')]
+			[ValidateSet('name','protocol','ports','description','comments','tags','custom_fields')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2,
 			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
