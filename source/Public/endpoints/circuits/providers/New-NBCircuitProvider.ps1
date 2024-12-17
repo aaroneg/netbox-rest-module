5c90b9f706d520cdf5370d81eee139f7bc8e4578
Aaron <aaron@aaroneg.com>
2024-12-16 18:00:57 -0800

    update circuitprovider schema

--- source/Public/endpoints/circuits/providers/New-NBCircuitProvider.ps1
+++ source/Public/endpoints/circuits/providers/New-NBCircuitProvider.ps1
@@ -2,7 +2,12 @@ function New-NBCircuitProvider {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
+		# Not adding support for accounts here, you can set those on the account
 		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][string]$comments,
+		[Parameter(Mandatory=$false)][string[]]$asns,
+		[Parameter(Mandatory=$false)][string[]]$tags,
+		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
 	$PSBoundParameters['slug']=makeSlug -name $name
