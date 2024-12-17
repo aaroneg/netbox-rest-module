8fe906251b3017f0b44c379e47e4177ce749267e
Aaron <aaron@aaroneg.com>
2024-12-16 16:23:26 -0800

    fix field names

--- source/Public/endpoints/dcim/devices/Set-NBDevice.ps1
+++ source/Public/endpoints/dcim/devices/Set-NBDevice.ps1
@@ -3,10 +3,12 @@ function Set-NBDevice {
 	param (
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
+		# ValidateSets updated as of Netbox v4.1.8
 		[Parameter(Mandatory=$true,Position=1)][string]
 			[ValidateSet('name','device_type','role','tenant','platform','serial','asset_tag','site',
-				'location','rack','position','face','parent_device','status','airflow','primary_ip4',
-				'primary_ip6','primary_ip','cluster','virtual_chassis','vc_position','vc_priority','comments','tags')]
+				'location','rack','position','face','latitude','longitude','status','airflow','primary_ip4',
+				'primary_ip6','oob_ip','cluster','virtual_chassis','vc_position','vc_priority','description',
+				'comments','tags','custom_fields')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2)][string]$value
 	)
