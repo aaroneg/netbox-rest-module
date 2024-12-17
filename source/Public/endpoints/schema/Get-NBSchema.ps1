e06844d5c32f8157060889c20f6aed272476aa0d
Aaron <aaron@aaroneg.com>
2024-12-17 09:16:30 -0800

    variable cleanup

--- source/Public/endpoints/schema/Get-NBSchema.ps1
+++ source/Public/endpoints/schema/Get-NBSchema.ps1
@@ -3,10 +3,6 @@ function Get-NBSchema {
 	param (
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
 	)
-	$restParams=@{
-		Method = 'Get'
-		Uri = "$($Connection.ApiBaseUrl)/schema/"
-	}	
 	(Get-ApiItemByQuery -apiConnection $Connection -RelativePath 'schema' -field format -value 'json')
 
 }
