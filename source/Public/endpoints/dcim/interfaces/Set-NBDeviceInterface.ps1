2471e8dea327bb555312d910f1524c5f7f4efe70
Aaron <aaron@aaroneg.com>
2026-03-01 21:03:55 -0800

    updates to device interfaces

--- source/Public/endpoints/dcim/interfaces/Set-NBDeviceInterface.ps1
+++ source/Public/endpoints/dcim/interfaces/Set-NBDeviceInterface.ps1
@@ -3,11 +3,7 @@ function Set-NBDeviceInterface {
 	param (
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
-		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('device','vdcs','module','name','label','type','enabled','parent','bridge','lag','mtu','mac_address','speed',
-				'duplex','wwn','mgmt_only','description','mode','rf_role','rf_channel','poe_mode','poe_type','rf_channel_frequency',
-				'rf_channel_width','tx_power','untagged_vlan','tagged_vlans','mark_connected','wireless_lans','vrf','tags','custom_fields')]
-			$key,
+		[Parameter(Mandatory=$true,Position=1)][string]$key, # There are a lot of attributes here that are possible - punting to the API itself to say whether it's a real attribute
 		[Parameter(Mandatory=$true,Position=2)][string]$value
 	)
 	$update=processFieldUpdates $key $value
