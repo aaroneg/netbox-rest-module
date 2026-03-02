61bed93fa2a0dfb93fcb77045f5a588ed9e5a042
Aaron <aaron@aaroneg.com>
2026-03-01 21:58:43 -0800

    add api paths

--- source/Public/endpoints/extras/object-types/Get-NBObjectTypeByID.ps1
+++
@@ -1,9 +0,0 @@
-function Get-NBObjectTypeByID {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
-		[Parameter(Mandatory=$true,Position=0)][int]$id
-	)
-	Get-ApiItemByID -apiConnection $Connection -RelativePath $NBObjectTypesAPIPath -id $id
-
-}
--- source/Public/endpoints/extras/object-types/Get-NBObjectTypes.ps1
+++
@@ -1,8 +0,0 @@
-function Get-NBObjectTypes {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
-	)
-	Get-ApiItems -apiConnection $Connection -RelativePath $NBObjectTypesAPIPath
-
-}
