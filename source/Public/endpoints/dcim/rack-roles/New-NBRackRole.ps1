e58f2ea16ad7aa4306df69cd723e63f6b82eea4f
Aaron <aaron@aaroneg.com>
2026-07-16 21:49:35 -0700

    rack role updates

--- source/Public/endpoints/dcim/rack-roles/New-NBRackRole.ps1
+++ source/Public/endpoints/dcim/rack-roles/New-NBRackRole.ps1
@@ -4,6 +4,8 @@ function New-NBRackRole {
 		[Parameter(Mandatory=$true,Position=0)][string]$name,
 		[Parameter(Mandatory=$false)][string]$color,
 		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][int]$owner,
+		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][string[]]$tags,
 		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection
