8f1dac7290063492115997201ad79d58e4d8eb0d
Aaron <aaron@aaroneg.com>
2026-07-16 21:46:56 -0700

    closes #21 - allows http only connections as working with a netbox container is convenient

--- source/Public/New-NBConnection.ps1
+++ source/Public/New-NBConnection.ps1
@@ -5,13 +5,15 @@ function New-NBConnection {
 		[Parameter(Mandatory=$true,Position=1)][string]$ApiKeyID,
 		[Parameter(Mandatory=$true,Position=2)][string]$ApiKey,
 		[Parameter(Mandatory=$false)][switch]$SkipCertificateCheck,
+		[Parameter(Mandatory=$false)][switch]$HttpOnly,
 		[Parameter(Mandatory=$false)][switch]$Passthru
 	)
+	if ($HttpOnly){$ConnScheme='http'} else {$ConnScheme='https'}
 	$ConnectionProperties = @{
 		Address = "$DeviceAddress"
 		ApiKeyID = $ApiKeyID
 		ApiKey = $ApiKey
-		ApiBaseUrl = "https://$($DeviceAddress)/api"
+		ApiBaseUrl = "$($ConnScheme)://$($DeviceAddress)/api"
 		SkipCertificateCheck = $SkipCertificateCheck
 	}
 	$Connection = New-Object psobject -Property $ConnectionProperties
