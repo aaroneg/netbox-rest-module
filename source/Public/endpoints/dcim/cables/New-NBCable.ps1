751ce020b0edc50e63b8225518d7242dac4de3a0
Aaron <aaron@aaroneg.com>
2026-07-25 22:09:53 -0700

    updates to cable api

--- source/Public/endpoints/dcim/cables/New-NBCable.ps1
+++ source/Public/endpoints/dcim/cables/New-NBCable.ps1
@@ -1,14 +1,67 @@
+<#
+.SYNOPSIS
+Create new Cable object
+
+.DESCRIPTION
+Create new Cable object
+
+.PARAMETER type
+The cable type, ex: cat3 - there are a lot of options for this, the API value is usually the lowercase version of the values displayed on the web page.
+
+.PARAMETER status
+Lifecycle status of the cable - connected, planned, or decommissioning
+
+.PARAMETER profile
+Cable type, like breakout, trunk, etc - the API values are not obvious from the web page, see the "Rest API documentation" link in your Netbox instance, search on the page for 'api/dcim/cables', click the "post" entry and expand the WritableCableRequest object down to the field level. All valid values for your version of Netbox are listed there.
+
+.PARAMETER tenant
+Tenant object ID
+
+.PARAMETER bundle
+Bundle object ID
+
+.PARAMETER label
+Label on cable
+
+.PARAMETER color
+Color code in Hex without "#" prefix, ex: FFFFFF
+
+.PARAMETER length
+Number of units this cable is in length, like "1". You'll set the measurement type in length_unit
+
+.PARAMETER length_unit
+How is the length measured? km,m,cm,mi,ft,in
+
+.PARAMETER description
+A description of the object
+
+.PARAMETER owner
+Owner object ID
+
+.PARAMETER comments
+Comments
+
+.PARAMETER tags
+Array of tag IDs
+
+.PARAMETER custom_fields
+A hashtable of custom fields & IDs
+
+.PARAMETER Connection
+The connection object to use, if not using default.
+#>
 function New-NBCable {
 	[CmdletBinding()]
 	param (
-		[Parameter(Mandatory=$false)]
-			[string]$type,
-		[Parameter(Mandatory=$true,Position=0)][hashtable[]]$a_terminations,
-		[Parameter(Mandatory=$true,Position=1)][hashtable[]]$b_terminations,
+		[Parameter(Mandatory=$false)][string]$type,	
+		#[Parameter(Mandatory=$true,Position=0)][hashtable[]]$a_terminations,
+		#[Parameter(Mandatory=$true,Position=1)][hashtable[]]$b_terminations,
 		[Parameter(Mandatory=$false)]
 			[ValidateSet('connected','planned','decommissioning')]
 			[string]$status,
+		[Parameter(Mandatory=$false)][string]$profile,
 		[Parameter(Mandatory=$false)][int]$tenant,
+		[Parameter(Mandatory=$false)][int]$bundle,
 		[Parameter(Mandatory=$false)][string]$label,
 		[Parameter(Mandatory=$false)][string]$color,
 		[Parameter(Mandatory=$false)][int]$length,
@@ -16,6 +69,7 @@ function New-NBCable {
 			[ValidateSet('km','m','cm','mi','ft','in')]
 			[string]$length_unit,
 		[Parameter(Mandatory=$false)][string]$description,
+		[Parameter(Mandatory=$false)][int]$owner,
 		[Parameter(Mandatory=$false)][string]$comments,
 		[Parameter(Mandatory=$false)][string[]]$tags,
 		[Parameter(Mandatory=$false)][hashtable]$custom_fields,
--- source/Public/endpoints/dcim/cables/Set-NBCable.ps1
+++ source/Public/endpoints/dcim/cables/Set-NBCable.ps1
@@ -1,10 +1,29 @@
+<#
+.SYNOPSIS
+Change properties of a cable
+
+.DESCRIPTION
+Change properties of a cable
+
+.PARAMETER Connection
+The connection object to use, if not using default.
+
+.PARAMETER id
+Object ID to change
+
+.PARAMETER key
+What field should be changed?
+
+.PARAMETER value
+What is the new value?
+#>
 function Set-NBCable {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$false)][object]$Connection=$Script:Connection,
 		[Parameter(Mandatory=$true,Position=0)][int]$id,
 		[Parameter(Mandatory=$true,Position=1)][string]
-			[ValidateSet('type','a_terminations','b_terminations','status','tenant','label','color','length','length_unit','description','comments','tags','custom_fields')]
+			[ValidateSet('type','status','profile','tenant','bundle','label','color','length','length_unit','description','comments','tags','custom_fields')]
 			$key,
 		[Parameter(Mandatory=$true,Position=2,
 			HelpMessage="A valid value for the attribute you want to change. If the expected value is an array, like for tags, pass it as '1,2' or whatever the ids of the tags you wish to set are."
